3f6487a31cc5650d74bb4718f214275d9650aefe
RvnSytR <ravenskytd17@gmail.com>
2026-06-03 08:40:56 +0700

    feat: prefer-nullish-coalescing lint rules

--- eslint.config.js
+++ eslint.config.js
@@ -8,17 +8,18 @@ export default defineConfig([
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     rules: {
-      // Enabled rules
+      // Enabled rules (warn)
       "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": "warn",
+
+      // Enabled rules (error)
       "@typescript-eslint/no-explicit-any": "error",
 
       // Disabled rules
       "import/order": "off",
       "sort-imports": "off",
-
       "no-extra-boolean-cast": "off",
       "@stylistic/spaced-comment": "off",
-
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/naming-convention": "off",
       "@typescript-eslint/consistent-type-imports": "off",
